04b29070b1120bacdb2ffd5ad2c70bfbbd926428
youth-o <127082479+youth-o@users.noreply.github.com>
2025-04-04 02:41:06 +0900

    [design]: 정렬 수정

--- src/features/challenge/ui/ChallengeCard.tsx
+++ src/features/challenge/ui/ChallengeCard.tsx
@@ -11,7 +11,7 @@ export default function ChallengeCard() {
 
   return (
     <div className="w-[280px] h-[111px] bg-yellow-300 p-4 flex flex-col gap-4 justify-center items-center rounded-lg">
-      <div className="flex justify-between gap-3 w-full h-[60px]">
+      <div className="flex justify-between items-center gap-3 w-full h-[60px]">
         <img src="/img/coffee-cup.svg" alt="커피컵" className="w-[60px] h-[60px] bg-green-400 rounded-lg" />
         <div className="flex flex-col justify-center gap-[10px]">
           <h5 className="text-[#254434] font-extrabold text-sm whitespace-nowrap">{challengeTitle}</h5>
