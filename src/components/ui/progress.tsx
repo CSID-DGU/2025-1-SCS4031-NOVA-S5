4ca8d70a20eca825bdd979fda53c3953b41eec08
youth-o <127082479+youth-o@users.noreply.github.com>
2025-04-03 16:46:25 +0900

    design: progress UI

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -2,22 +2,42 @@
 
 import * as React from "react";
 import * as ProgressPrimitive from "@radix-ui/react-progress";
+import Image from "next/image";
+import characterIcon from "@/assets/main/yellow-face.svg";
 
 import { cn } from "@/lib/utils";
 
-function Progress({ className, value, ...props }: React.ComponentProps<typeof ProgressPrimitive.Root>) {
+interface ProgressProps extends React.ComponentProps<typeof ProgressPrimitive.Root> {
+  value?: number;
+  character?: boolean;
+}
+
+function Progress({ className, value = 0, character = false, ...props }: ProgressProps) {
   return (
-    <ProgressPrimitive.Root
-      data-slot="progress"
-      className={cn("bg-green-400 relative h-3 w-full overflow-hidden rounded-full", className)}
-      style={{ backgroundColor: "#EFF4E5" }}
-      {...props}>
-      <ProgressPrimitive.Indicator
-        data-slot="progress-indicator"
-        className="bg-green-100 h-full w-full flex-1 transition-all"
-        style={{ transform: `translateX(-${100 - (value || 0)}%)`, backgroundColor: "#B5CDB7" }}
-      />
-    </ProgressPrimitive.Root>
+    <div className="relative w-full h-[24px]">
+      <ProgressPrimitive.Root
+        data-slot="progress"
+        className={cn("bg-green-400 relative h-1 w-full overflow-hidden rounded-full", className)}
+        style={{ backgroundColor: "#EFF4E5" }}
+        {...props}>
+        <ProgressPrimitive.Indicator
+          data-slot="progress-indicator"
+          className="bg-green-100 h-full flex items-center transition-all"
+          style={{
+            transform: `translateX(-${100 - value}%)`,
+            backgroundColor: "#B5CDB7",
+          }}></ProgressPrimitive.Indicator>
+      </ProgressPrimitive.Root>
+      {character && (
+        <div
+          className="absolute -top-3 transition-all"
+          style={{
+            left: `calc(${value}% - 20px)`,
+          }}>
+          <Image src={characterIcon} alt="face" width={23} height={23} />
+        </div>
+      )}
+    </div>
   );
 }
 
