d8902c61e643f8874c0c362b05c6cf3faa958a85
youth-o <127082479+youth-o@users.noreply.github.com>
2025-04-04 02:27:33 +0900

    [style]: 이미지 태그 변경

--- src/features/challenge/ui/ChallengeCard.tsx
+++ src/features/challenge/ui/ChallengeCard.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import coffee from "../../../../public/img/coffee-cup.svg";
-import arrowRight from "../../../../public/icon/arrow-right.svg";
 import { Progress } from "@/components/ui/progress";
 
 const mockupData = {
@@ -15,7 +12,7 @@ export default function ChallengeCard() {
   return (
     <div className="w-[280px] h-[111px] bg-yellow-300 p-4 flex flex-col gap-4 justify-center items-center rounded-lg">
       <div className="flex justify-between gap-3 w-full h-[60px]">
-        <Image src={coffee} alt="커피컵" width={60} height={60} className="bg-green-400 rounded-lg" />
+        <img src="/img/coffee-cup.svg" alt="커피컵" width={60} height={60} className="bg-green-400 rounded-lg" />
         <div className="flex flex-col justify-center gap-[10px]">
           <h5 className="text-[#254434] font-extrabold text-sm whitespace-nowrap">{challengeTitle}</h5>
           <div className="flex gap-1">
@@ -23,7 +20,7 @@ export default function ChallengeCard() {
             <p className="text-[#254434] text-opacity-50 text-xs">/ {totalDay}일 진행 중!</p>
           </div>
         </div>
-        <Image src={arrowRight} alt="오른쪽화살표" width={20} height={20} className="cursor-pointer ml-5" />
+        <img src="/icon/arrow-right.svg" alt="오른쪽화살표" width={20} height={20} className="cursor-pointer ml-5" />
       </div>
       <Progress value={currentDay * 10} character={false} />
     </div>
--- src/features/stampBook/ui/StampCard.tsx
+++ src/features/stampBook/ui/StampCard.tsx
@@ -1,9 +1,3 @@
-"use client";
-
-import Image from "next/image";
-import yellowCharacter from "../../../../public/img/yellow-all.svg";
-import orangeCharacter from "../../../../public/img/orange-all.svg";
-import coffee from "../../../../public/icon/coffee.svg";
 import { Progress } from "@/components/ui/progress";
 
 const mockupData = {
@@ -18,22 +12,22 @@ export default function StampCard() {
 
   return (
     <div className="relative w-[280px] h-[149px] bg-yellow-300 p-4 flex flex-col gap-4 justify-center rounded-lg overflow-hidden">
-      <Image
-        src={yellowCharacter}
+      <img
+        src="/img/yellow-all.svg"
         width={37}
         height={51}
         alt="토끼 캐릭터"
         className="absolute right-[-10px] top-[2px] rotate-[-24deg] opacity-20"
       />
-      <Image
-        src={orangeCharacter}
+      <img
+        src="/img/orange-all.svg"
         width={37}
         height={47}
         alt="여우 캐릭터"
         className="absolute right-[-15px] top-[50px] rotate-[-54deg] opacity-20"
       />
       <div className="flex gap-[6px]">
-        <Image src={coffee} width={16} height={16} alt="원두" />
+        <img src="/icon/coffee.svg" width={16} height={16} alt="원두" />
         <p className="text-[#254434] text-sm font-bold">{cafeName}</p>
       </div>
       <div className="flex flex-col gap-[7px] mt-4">
