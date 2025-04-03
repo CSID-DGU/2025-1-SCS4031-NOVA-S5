3d0fcadb49740fb4dee4c865f0a982cea8b38e0a
youth-o <127082479+youth-o@users.noreply.github.com>
2025-04-03 16:20:52 +0900

    design: progress UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,8 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="flex gap-2">
-      <Button className="bg-amber-800">
-        button
-      </Button>
-      <Button>
-        button
-      </Button>
+      <Button className="bg-amber-800">button</Button>
+      <Button>button</Button>
     </div>
   );
 }
