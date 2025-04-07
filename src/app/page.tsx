b3785a4bdbe87b189e4b3314356cc38fb96eb190
RohSungKyun <songkyun1108@naver.com>
2025-04-08 00:50:06 +0900

    [fix] #1 : 헤더 레이아웃에서 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import FortuneCard from "@/features/fortuneWidget/ui/FortuneCard";
 import UserCard from "@/features/qrScanner/ui/UserCard";
-
+import Header from "@/shared/ui/Header";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-2 items-center">
-      <FortuneCard />
-      <UserCard />
+    <div>
+      <div className="flex flex-col gap-2 items-center">
+        <FortuneCard />
+        <UserCard />
+      </div>      
     </div>
+
   );
 }
--- src/shared/ui/Header.tsx
+++ src/shared/ui/Header.tsx
@@ -1,21 +1,17 @@
-import Link from 'next/link';
 import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header className="header">
-      <div className="header-container">
-        <Link href="/" className="logo-container">
+    <header className="relative h-[50px] z-[1000] bg-[#FBFBEE] max-w-[375px] mx-auto pt-[10px]">
+      <div className="px-2 pb-2 h-full flex items-center justify-between">
+        <div className="flex items-center pl-4">
           <Image 
             src="./icon/ecok-logo.svg" 
             alt="로고" 
             width={80} 
             height={24} 
-            className="logo"
+            className="object-contain"
           />
-        </Link>
-        <div className="header-right">
-          {/* 필요한 경우 오른쪽에 추가 요소 배치 */}
         </div>
       </div>
     </header>
