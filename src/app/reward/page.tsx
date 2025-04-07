0119497a69d98c1dea0dbfca7374758b8a1a7c0a
youth-o <127082479+youth-o@users.noreply.github.com>
2025-04-07 20:23:30 +0900

    [feat]: #7 스탬프북 컴포넌트 구현

--- src/app/reward/page.tsx
+++ src/app/reward/page.tsx
@@ -1,3 +1,9 @@
+import StampBook from "@/shared/ui/StampBook";
+
 export default function Reward() {
-  return <div className="flex flex-col gap-2"></div>;
+  return (
+    <div className="flex flex-col gap-2">
+      <StampBook stampBookId={2} />
+    </div>
+  );
 }
---
+++ src/shared/ui/StampBook.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { useStampBookStore } from "../store/stampBookStore";
+
+interface StampCardProps {
+  stampBookId: number;
+  characterType?: "yellow" | "green" | "orange" | "beige";
+}
+
+export default function StampBook({ stampBookId, characterType = "yellow" }: StampCardProps) {
+  const book = useStampBookStore(state => state.stampBooks.find(b => b.id === stampBookId));
+  if (!book) return <p>스탬프북을 찾을 수 없어요</p>;
+
+  const { cafeName, totalStamp, remainingStamp } = book;
+  const stampedCount = totalStamp - remainingStamp;
+  const stampedSrc = `/img/character/${characterType}-face.svg`;
+
+  return (
+    <div className="w-[320px] h-[154px] flex flex-col gap-4 py-5 px-[18px] bg-yellow-300 rounded-lg">
+      <div className="flex gap-[6px] items-center">
+        <img src="/icon/coffee.svg" alt="원두" className="w-[16px] h-[16px]" />
+        <p className="text-sm font-bold text-[#254434]">{cafeName}</p>
+      </div>
+      <div className="grid grid-cols-5 gap-x-[20px] gap-y-3 px-[15px]">
+        {Array.from({ length: totalStamp }).map((_, index) => (
+          <img
+            key={index}
+            src={index < stampedCount ? stampedSrc : "/img/character/yellow-gray.svg"}
+            alt={index < stampedCount ? "스탬프 찍힘" : "스탬프 안 찍힘"}
+            className="w-[35px] h-[35px]"
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
