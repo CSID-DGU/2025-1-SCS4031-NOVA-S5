9bad1fa9faa9ee54158dd5bed6a209d3f8855144
youth-o <127082479+youth-o@users.noreply.github.com>
2025-04-08 18:10:05 +0900

    [feat]: #7 GNB 이동 기능 추가

--- src/shared/ui/GNB.tsx
+++ src/shared/ui/GNB.tsx
@@ -1,15 +1,24 @@
+"use client";
+import { useRouter } from "next/navigation";
+
 export default function GNB() {
+  const router = useRouter();
+
   return (
     <div className="bg-yellow-300 px-[27px] py-[18px] flex justify-between items-center shadow-[0px -6px 14px 0px rgba(47, 47, 47, 0.04)]">
       <div className="flex flex-col gap-1 items-center justify-center cursor-pointer">
         <img src="/icon/magazine.svg" alt="매거진" className="w-[26px] h-[26px]" />
         <p className="text-[#9C9CA1B2] text-[10px]">매거진</p>
       </div>
-      <div className="flex flex-col gap-1 items-center justify-center cursor-pointer">
+      <div
+        className="flex flex-col gap-1 items-center justify-center cursor-pointer"
+        onClick={() => router.push("/reward")}>
         <img src="/icon/reward-history.svg" alt="적립내역" className="w-[26px] h-[26px]" />
         <p className="text-[#9C9CA1B2] text-[10px]">적립 내역</p>
       </div>
-      <div className="flex flex-col gap-1 items-center justify-center cursor-pointer">
+      <div
+        className="flex flex-col gap-1 items-center justify-center cursor-pointer"
+        onClick={() => router.push("/main")}>
         <img src="/icon/home.svg" alt="메인" className="w-[26px] h-[26px]" />
         <p className="text-[#9C9CA1B2] text-[10px]">홈</p>
       </div>
