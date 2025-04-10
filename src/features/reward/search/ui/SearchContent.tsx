ff96117c807ec021e0d65a6c1d79f70945f17e35
youth-o <127082479+youth-o@users.noreply.github.com>
2025-04-10 16:14:55 +0900

    [feat]: #14 적립내역 - 검색페이지 구현

--- src/features/reward/search/ui/SearchContent.tsx
+++ src/features/reward/search/ui/SearchContent.tsx
@@ -1,10 +1,28 @@
+"use client";
+
 import SearchBar from "@/shared/ui/input/SearchBar";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function SearchContent() {
+  const router = useRouter();
+
   return (
-    <div className="w-full flex gap-[10px] items-center">
-      <img src="/icon/arrow-left.svg" alt="왼쪽 화삺표" className="w-[30px] h-[30px]" />
-      <SearchBar />
+    <div className="w-full flex flex-col items-center gap-[226px]">
+      <div className="w-full flex gap-[10px] items-center">
+        <Image
+          src={"/icon/arrow-left.svg"}
+          alt="왼쪽 화살표"
+          width={30}
+          height={30}
+          onClick={() => router.push("/reward")}
+        />
+        <SearchBar placeholder="어떤 카페에 스탬프를 적립했을까요?" />
+      </div>
+      <div className="flex flex-col items-center gap-[38px]">
+        <Image src={"/img/character/beige-ear.svg"} alt="캐릭터" width={124} height={94} />
+        <p className="text-font-black text-md font-bold">어떤 스탬프북을 구경하러 가볼까요?</p>
+      </div>
     </div>
   );
 }
