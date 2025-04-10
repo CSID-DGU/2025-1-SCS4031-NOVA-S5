06c42aafd5eb6ec5031779a0e740f357e8b5ae4a
RohSungKyun <songkyun1108@naver.com>
2025-04-10 16:33:50 +0900

    [feat]: #15 서치바 구현

--- src/features/stampbook/ui/SearchBar.tsx
+++ src/features/stampbook/ui/SearchBar.tsx
@@ -3,10 +3,12 @@ import { Input } from "@/components/ui/input";
 
 function SearchBar() {
   return (
-    <div>
+    <div className="flex flex-row items-center bg-green-4 rounded-full w-[310px] h-[35px]">
+      <img src="/icon/search.svg" alt="search" className="w-[16px] h-[17px] ml-3"/>
       <Input 
-        placeholder="검색"
-        className="w-[310px] h-[35px] rounded-full"
+        placeholder="방문할 카페를 검색해 보세요!"
+        className="rounded-full border-none !focus:outline-none !focus:ring-0 !focus-visible:outline-none !focus-visible:ring-0 !focus-visible:border-input placeholder:text-green-font"
+        style={{ outline: 'none', boxShadow: 'none' }}
       />
     </div>
   );
