86931b1b2d8102e6acd73862ed5f99c88ad8f1ad
RohSungKyun <songkyun1108@naver.com>
2025-04-15 13:46:37 +0900

    [fix]:  #15 바텀시트 패딩값 조정

--- src/features/viewStampList/ui/BottomSheet/Content.tsx
+++ src/features/viewStampList/ui/BottomSheet/Content.tsx
@@ -2,5 +2,5 @@ import { HTMLAttributes } from "react";
 import clsx from "clsx";
 
 export const Content = ({ className, ...props }: HTMLAttributes<HTMLDivElement>) => {
-  return <div className={clsx("p-4 overflow-y-auto max-h-[70vh]", className)} {...props} />;
+  return <div className={clsx("p-7 overflow-y-auto max-h-[70vh]", className)} {...props} />;
 };
