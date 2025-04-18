9f692607d97ea8d5d93cd5c43c128ac6f2c86d41
RohSungKyun <songkyun1108@naver.com>
2025-04-19 01:50:49 +0900

    [feat] : #21 카페 리워드 카드 구현

---
+++ src/features/cafeDetail/ui/CafeCharacter.tsx
@@ -0,0 +1,11 @@
+function CafeCharacter() {
+  return (
+    <div>
+      <p className="text-[16px] font-[800] text-font-green">
+        👀 어떤 캐릭터가 스탬프를 찍어줄까요?
+      </p>
+    </div>
+  );
+}
+
+export default CafeCharacter;
--- src/features/cafeDetail/ui/CafeContent.tsx
+++ src/features/cafeDetail/ui/CafeContent.tsx
@@ -1,21 +1,27 @@
 import { useCafeStore } from "@/shared/store/cafeDetailStore";
 import CafeInfo from "@/shared/ui/CafeInfo";
+import RewardCard from "./RewardCard";
+import CafeCharacter from "./CafeCharacter";
 
 function CafeContent() {
   const cafe = useCafeStore(state => state.cafe);
 
   return (
     <div className="m-[18px]">
-      <div className="flex flex-row gap-1">
+      <div className="flex flex-row gap-1 mb-5">
         <img src="/icon/info.svg" alt="info" />
         <p className="text-font-green text-[16px] font-[800]">Cafe Information</p>
       </div>
-      <CafeInfo
-        name={cafe?.name}
-        hours={cafe?.business_hour}
-        phone={cafe?.tel}
-        address={cafe?.address}
-      />
+      <div className="flex flex-col gap-[30px]">
+        <CafeInfo
+          name={cafe?.name}
+          hours={cafe?.business_hour}
+          phone={cafe?.tel}
+          address={cafe?.address}
+        />
+        <RewardCard />
+        <CafeCharacter />
+      </div>
     </div>
   );
 }
