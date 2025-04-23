f36b3faefa027b1328e3c9e634d758d6f3966863
youth-o <127082479+youth-o@users.noreply.github.com>
2025-04-23 18:02:30 +0900

    [feat]: #22 검색 결과 디바운싱 추가

--- src/features/reward/search/ui/SearchContent.tsx
+++ src/features/reward/search/ui/SearchContent.tsx
@@ -4,17 +4,28 @@ import SearchBar from "@/shared/ui/input/SearchBar";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useStampBookStore } from "@/shared/store/stampBookStore";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SearchResult from "./SearchResult";
 
 export default function SearchContent() {
   const router = useRouter();
   const [query, setQuery] = useState("");
+  const [debouncedQuery, setDebouncedQuery] = useState(query);
   const stampBooks = useStampBookStore(state => state.stampBooks);
 
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedQuery(query);
+    }, 500);
+
+    return () => clearTimeout(handler);
+  }, [query]);
+
   const filteredBooks = stampBooks.filter(book =>
-    book.cafeName.toLowerCase().includes(query.toLowerCase())
+    book.cafeName.toLowerCase().includes(debouncedQuery.toLowerCase())
   );
+
+  const hasQuery = debouncedQuery.trim() !== "";
 
   return (
     <section className="w-full flex flex-col items-center gap-[30px]">
@@ -34,18 +45,27 @@ export default function SearchContent() {
         />
       </div>
 
-      {query.trim() === "" ? (
+      {hasQuery ? (
+        filteredBooks.length > 0 ? (
+          <div className="flex flex-col gap-[20px]">
+            {filteredBooks.map(book => (
+              <SearchResult key={book.id} stampBookId={book.id} />
+            ))}
+          </div>
+        ) : (
+          <div className="flex flex-col items-center gap-[38px] mt-[226px]">
+            <Image src={"/img/character/beige-ear.svg"} alt="캐릭터" width={124} height={94} />
+            <p className="text-font-black text-md font-bold flex flex-col items-center">
+              '{debouncedQuery}'와 일치하는 검색 결과가 없어요
+              <br />
+              <span className="text-[14px]">다른 키워드로 검색해 보세요</span>
+            </p>
+          </div>
+        )
+      ) : (
         <div className="flex flex-col items-center gap-[38px] mt-[226px]">
           <Image src={"/img/character/beige-ear.svg"} alt="캐릭터" width={124} height={94} />
           <p className="text-font-black text-md font-bold">어떤 스탬프북을 구경하러 가볼까요?</p>
-        </div>
-      ) : filteredBooks.length === 0 ? (
-        <p className="text-sm mt-[100px] text-[#8A8A8A]">검색 결과가 없습니다.</p>
-      ) : (
-        <div className="flex flex-col gap-[20px]">
-          {filteredBooks.map(book => (
-            <SearchResult key={book.id} stampBookId={book.id} />
-          ))}
         </div>
       )}
     </section>
