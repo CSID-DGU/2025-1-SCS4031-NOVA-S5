c56c5badf78eefc3b06549e9cb5a924eb6dc47cd
youth-o <127082479+youth-o@users.noreply.github.com>
2025-05-02 23:06:00 +0900

    [feat]: #30 Dropdown 컴포넌트 구현

--- src/app/owner/main/page.tsx
+++ src/app/owner/main/page.tsx
@@ -1,9 +1,12 @@
 import { Input } from "@/components/ui/input";
+import { MoodDropdown, TimeDropdown } from "@/shared/ui/Dropdown";
 
 export default function OwnerMain() {
   return (
     <>
       <Input label="* 지점명" />
+      <TimeDropdown />
+      <MoodDropdown />
     </>
   );
 }
