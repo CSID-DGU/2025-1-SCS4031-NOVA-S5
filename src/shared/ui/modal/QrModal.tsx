7c35619b2779bf1e24d1f991538b1356c361b9de
RohSungKyun <songkyun1108@naver.com>
2025-05-05 20:28:18 +0900

    [feat] : #28 qrmodal props 추가

--- src/shared/ui/modal/QrModal.tsx
+++ src/shared/ui/modal/QrModal.tsx
@@ -1,21 +1,18 @@
 "use client";
 
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-} from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import QrSection from "./QrSection";
 import { useQuery } from "@tanstack/react-query";
 import { fetchQrValue } from "@/shared/api";
+import { ReactNode } from "react";
 interface QrModalProps {
+  userName?: ReactNode;
+  title?: ReactNode;
   isOpen: boolean;
   setIsOpen: (open: boolean) => void;
 }
 
-function QrModal({ isOpen, setIsOpen }: QrModalProps) {
+function QrModal({ title, userName, isOpen, setIsOpen }: QrModalProps) {
   const { data: qrValue } = useQuery({
     queryKey: ["qrValue"],
     queryFn: fetchQrValue,
@@ -25,15 +22,13 @@ function QrModal({ isOpen, setIsOpen }: QrModalProps) {
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogContent className="p-0 bg-[#FFFDF7] w-[340px] h-[500px] gap-0">
-        <DialogHeader className="bg-[#E2ECDC] flex justify-center items-center rounded-t-[10px]">
+        <DialogHeader className="bg-[#E2ECDC] flex justify-center items-center rounded-t-[10px] h-[150px]">
           <DialogTitle className="text-center !text-title-medium !font-[800] text-[#254434]">
-            kimnova님의
-            <br /> 프로필 QR
+            {title ?? userName}
           </DialogTitle>
-          <DialogDescription></DialogDescription>
         </DialogHeader>
         <div className="flex justify-center items-center">
-          <img src="/img/spring.svg" alt="spring" className="absolute top-[147px]" />
+          <img src="/img/spring.svg" alt="spring" className="absolute top-[135px]" />
         </div>
         <div className="flex flex-col  items-center">
           <QrSection uuid={qrValue} />
