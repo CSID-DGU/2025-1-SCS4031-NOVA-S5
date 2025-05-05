e3a62b025f884217513e587c70a1ce185c04b922
RohSungKyun <songkyun1108@naver.com>
2025-05-05 18:36:44 +0900

    [feat] : #28 qr 생성하기

--- src/shared/ui/modal/QrModal.tsx
+++ src/shared/ui/modal/QrModal.tsx
@@ -1,13 +1,27 @@
 "use client";
 
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import QrSection from "./QrSection";
+import { useQuery } from "@tanstack/react-query";
+import { fetchQrValue } from "@/shared/api";
 interface QrModalProps {
   isOpen: boolean;
   setIsOpen: (open: boolean) => void;
 }
 
 function QrModal({ isOpen, setIsOpen }: QrModalProps) {
+  const { data: qrValue } = useQuery({
+    queryKey: ["qrValue"],
+    queryFn: fetchQrValue,
+    enabled: isOpen, // 모달이 열릴 떄만 요청한다.
+  });
+
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogContent className="p-0 bg-[#FFFDF7] w-[340px] h-[500px] gap-0">
@@ -16,12 +30,13 @@ function QrModal({ isOpen, setIsOpen }: QrModalProps) {
             kimnova님의
             <br /> 프로필 QR
           </DialogTitle>
+          <DialogDescription></DialogDescription>
         </DialogHeader>
         <div className="flex justify-center items-center">
-          <img src="/img/spring.svg" alt="spring" className="absolute top-[145px]" />
+          <img src="/img/spring.svg" alt="spring" className="absolute top-[147px]" />
         </div>
         <div className="flex flex-col  items-center">
-          <QrSection />
+          <QrSection uuid={qrValue} />
         </div>
       </DialogContent>
     </Dialog>
--- src/shared/ui/modal/QrSection.tsx
+++ src/shared/ui/modal/QrSection.tsx
@@ -1,10 +1,11 @@
-function QrSection() {
-  // qr 이미지 url을 props로 받아옴
+import { QRCodeCanvas } from "qrcode.react";
+
+function QrSection({ uuid }: { uuid: string }) {
   return (
-    <div className="flex flex-col items-center">
-      <img src="/img/scan-rabbit.svg" alt="scan-rabbit" className="absolute top-[215px]" />
+    <div className="relative flex flex-col items-center">
+      <img src="/img/scan-rabbit.svg" alt="scan-rabbit" className="absolute top-[-30px]" />
       <img src="/img/qr-section.svg" alt="qr-section" />
-      <img src="/img/mock-qr.svg" alt="mock-qr" className="absolute top-[275px]" />
+      <QRCodeCanvas value={uuid} size={110} className="absolute top-6" />
     </div>
   );
 }
