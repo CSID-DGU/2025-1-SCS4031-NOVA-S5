2efab0b0df185265d3deb9c40f96b362a312948e
RohSungKyun <songkyun1108@naver.com>
2025-05-11 01:12:55 +0900

    [chore] : #32 main페이지 이동

--- src/app/owner/main/page.tsx
+++ src/app/owner/main/page.tsx
@@ -1,12 +1,18 @@
-import { Input } from "@/components/ui/input";
-import { MoodDropdown, TimeDropdown } from "@/shared/ui/Dropdown";
+import { AddCafeCard, AddStampbook, DashBoardHeader, QrCard } from "@/features/owner/ui";
+import { OwnerGNB } from "@/shared";
 
 export default function OwnerMain() {
   return (
-    <>
-      <Input label="* 지점명" />
-      <TimeDropdown />
-      <MoodDropdown />
-    </>
+    <div className="p-5">
+      <DashBoardHeader title="스탬프 적립" />
+      <div className="mt-8">
+        {/* <AddCafeCard status="none" /> */}
+        {/* <AddStampbook /> */}
+        <QrCard />
+      </div>
+      <div className="absolute bottom-0 left-0 right-0">
+        <OwnerGNB />
+      </div>
+    </div>
   );
 }
