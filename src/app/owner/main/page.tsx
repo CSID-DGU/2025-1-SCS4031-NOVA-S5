8435d49e40bf215a9b8d1c69d55377b7bf0bb062
RohSungKyun <songkyun1108@naver.com>
2025-05-11 01:34:35 +0900

    [feat] : #32 사장페이지 카드 분기

--- src/app/owner/main/page.tsx
+++ src/app/owner/main/page.tsx
@@ -2,14 +2,39 @@ import { AddCafeCard, AddStampbook, DashBoardHeader, QrCard } from "@/features/o
 import { OwnerGNB } from "@/shared";
 
 export default function OwnerMain() {
+  const mockCafeList = [
+    {
+      id: 1,
+      name: "카페1",
+      chooseStatus: false,
+      confirm_status: "pending",
+      exist_stampbook: false,
+    },
+    {
+      id: 2,
+      name: "카페2",
+      chooseStatus: true,
+      confirm_status: "confirmed",
+      exist_stampbook: true,
+    },
+  ];
+
+  const selectedCafe = mockCafeList.find(cafe => cafe.chooseStatus);
+
+  let content = null;
+
+  if (mockCafeList.length === 0) {
+    content = <AddCafeCard status="none" />;
+  } else if (selectedCafe?.confirm_status === "pending") {
+    content = <AddCafeCard status="pending" />;
+  } else if (selectedCafe?.confirm_status === "confirmed") {
+    content = selectedCafe.exist_stampbook ? <QrCard /> : <AddStampbook />;
+  }
+
   return (
     <div className="p-5">
       <DashBoardHeader title="스탬프 적립" />
-      <div className="mt-8">
-        {/* <AddCafeCard status="none" /> */}
-        {/* <AddStampbook /> */}
-        <QrCard />
-      </div>
+      <div className="mt-8">{content}</div>
       <div className="absolute bottom-0 left-0 right-0">
         <OwnerGNB />
       </div>
