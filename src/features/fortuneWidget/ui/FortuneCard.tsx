baa55d105927579b1b1986e9e853cc825e511901
RohSungKyun <songkyun1108@naver.com>
2025-05-12 18:16:33 +0900

    [chore] : #36 경로 수정

--- src/features/fortuneWidget/ui/FortuneCard.tsx
+++ src/features/fortuneWidget/ui/FortuneCard.tsx
@@ -3,7 +3,7 @@
 function FortuneCard() {
   return (
     <div className="flex items-center gap-2 bg-[#E2ECDC] w-[330px] h-[65px] rounded-[10px]">
-      <img src="./fortuneCharacter.svg" alt="fortune-character" className="p-2" />
+      <img src="/fortuneCharacter.svg" alt="fortune-character" className="p-2" />
       <div className="flex flex-col gap-1">
         <p className="text-[14px] font-[700] text-[#254434]">비니가 알려주는 오늘의 환경 운세</p>
         <p className="text-[10px] text-green-800">자연이 당신을 지켜보고 있습니다! 오늘은 대중교통을 이용해보세요.</p>
@@ -11,5 +11,6 @@ function FortuneCard() {
     </div>
   )
 }
+
 export default FortuneCard;
 
