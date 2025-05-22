7e5d6b7d656ba753878f6f5bf6baa414215730cd
RohSungKyun <songkyun1108@naver.com>
2025-05-22 14:56:28 +0900

    [feat] : #44 카페 필터링 훅 구현

--- src/features/viewStampList/ui/ViewSwitcher.tsx
+++ src/features/viewStampList/ui/ViewSwitcher.tsx
@@ -3,15 +3,22 @@ import TabBar from "./TabBar";
 import ListView from "./ViewMode/ListView";
 import MapView from "./ViewMode/MapView";
 import { useCafes } from "../hooks/useCafes";
+import { useCurrentLocation } from "../hooks/useCurrentLocation";
+import { filterCafe } from "../utils";
 
 const ViewSwitcher = () => {
   const { viewMode } = useViewModeStore();
-
+  const { lat, lng } = useCurrentLocation(); // 사용자 위치 얻기
   const { data, isLoading, isError } = useCafes({ approved: true });
 
   if (isLoading) return <div className="p-4">카페 정보를 불러오는 중입니다...</div>;
   if (isError) return <div className="p-4">카페 정보를 불러오지 못했습니다.</div>;
   if (!data) return null;
+  if (lat === null || lng === null)
+    return <div className="p-4">위치 정보를 불러오는 중입니다...</div>;
+
+  const filtered = filterCafe(data, lat, lng);
+  console.log(filtered);
 
   return (
     <div className="pb-[80px]">
