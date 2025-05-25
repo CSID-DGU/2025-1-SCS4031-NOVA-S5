0231434b8ade1eb63b1ee47e532d8f0bd26a0fe5
RohSungKyun <songkyun1108@naver.com>
2025-05-25 21:50:56 +0900

    [feat] : #44 검색 기능 구현

--- src/shared/ui/InfoCard.tsx
+++ src/shared/ui/InfoCard.tsx
@@ -14,7 +14,7 @@ function InfoCard(info: InfoCardProps) {
   const router = useRouter();
   return (
     <div
-      className="flex flex-row gap-2 pl-3 items-center bg-[#FBFBEE] w-full h-[100px] rounded-[10px] cursor-pointer"
+      className="flex flex-row gap-2 pl-3 items-center bg-yellow-300 w-full h-[100px] rounded-[10px] cursor-pointer shadow-md hover:shadow-lg transition-shadow duration-200"
       onClick={() => router.push(`/stamplist/${info.id}`)}>
       <img src={info.img_url || "/default-cafe.jpg"} className="w-[80px] h-[80px] rounded-[10px]" />
       <div className="flex flex-col gap-1">
