d6b130ab856343dbe0b30f14e168753c736e2094
RohSungKyun <songkyun1108@naver.com>
2025-05-25 18:29:16 +0900

    [feat] : #44 인기있는 카페 api 연결

--- src/features/viewStampList/service/api.ts
+++ src/features/viewStampList/service/api.ts
@@ -5,3 +5,8 @@ export const getCafes = async (params?: GetCafesParams): Promise<CafeResponse[]>
   const response = await api.get("/cafes", { params });
   return response.data.data;
 };
+
+export const getPopularCafes = async () => {
+  const response = await api.get("/cafes/popular");
+  return response.data.data;
+};
