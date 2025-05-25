3b0e89d5995388afaef449db5ab436ff4fa44b2b
RohSungKyun <songkyun1108@naver.com>
2025-05-25 18:11:08 +0900

    [feat] : #44 LocationCard props 정리

--- src/shared/store/cafeStore.ts
+++ src/shared/store/cafeStore.ts
@@ -1,18 +1,17 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
 export interface Cafe {
   cafeId: number;
   cafeName: string;
   cafeImage?: string;
+  branchName: string;
+  cafeIntroduction: string;
+  conceptIntroduction: string;
+  maxStampCount: number;
   latitude: number;
   longitude: number;
   cafePhone: string | null;
-  maxStampCount: number;
-  isOpenNow: boolean;
-  openHours: any[];
-  specialDays: any[];
-  downloadCount: number;
-
+  characterType: "BLACK" | "ORANGE" | "YELLOW" | "GREEN"; // enum이면 더 명확하게 지정 가능
 }
 
 interface CafeStore {
@@ -20,7 +19,7 @@ interface CafeStore {
   setCafes: (cafes: Cafe[]) => void;
 }
 
-export const useCafeStore = create<CafeStore>((set) => ({
+export const useCafeStore = create<CafeStore>(set => ({
   cafes: [],
-  setCafes: (cafes) => set({ cafes }),
-})); 
+  setCafes: cafes => set({ cafes }),
+}));
