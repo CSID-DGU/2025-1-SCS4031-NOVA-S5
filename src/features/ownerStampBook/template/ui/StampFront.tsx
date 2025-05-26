1de1097409c3ec0022c3efe9c843bba519039a8d
youth-o <127082479+youth-o@users.noreply.github.com>
2025-05-27 03:04:18 +0900

    bug: 템플릿에서 매장명 수정시 동기화

--- src/features/ownerStampBook/template/ui/StampFront.tsx
+++ src/features/ownerStampBook/template/ui/StampFront.tsx
@@ -1,19 +1,32 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import OwnerStampBook from "../../ui/OwnerStampBook";
-import { useState } from "react";
+import TemplateStampBook from "./TemplateStampBook";
+import { useEffect, useState } from "react";
 import InputModal from "@/shared/ui/modal/InputModal";
+import { useSelectedCafe } from "@/shared/hooks/useSelectedCafe";
 
 export default function StampFront() {
   const [isOpen, setIsOpen] = useState(false);
+  const { selectedCafe } = useSelectedCafe();
+  const [frontName, setFrontName] = useState(selectedCafe?.cafeName || "");
+
+  const handleNameChange = (name: string) => {
+    setFrontName(name);
+  };
+
+  useEffect(() => {
+    if (selectedCafe?.cafeName && !frontName) {
+      setFrontName(selectedCafe.cafeName);
+    }
+  }, [selectedCafe, frontName, setFrontName]);
 
   return (
     <>
       <div className="flex flex-col gap-4">
         <p className="text-md text-font-green font-bold">앞면</p>
         <div className="flex items-center justify-center">
-          <OwnerStampBook />
+          <TemplateStampBook frontName={frontName} />
         </div>
         <Button
           className="w-[109px] rounded-full bg-green-300 text-font-green text-sm font-semibold mx-auto"
@@ -21,7 +34,12 @@ export default function StampFront() {
           매장명 바꾸기
         </Button>
       </div>
-      <InputModal isOpen={isOpen} setIsOpen={setIsOpen} target="front" />
+      <InputModal
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        target="front"
+        onNameChange={handleNameChange}
+      />
     </>
   );
 }
--- src/features/ownerStampBook/template/ui/TemplateStampBook.tsx
+++ src/features/ownerStampBook/template/ui/TemplateStampBook.tsx
@@ -3,10 +3,12 @@
 import Image from "next/image";
 import { useSelectedCafe } from "@/shared/hooks/useSelectedCafe";
 
-// 추후에 현재 선택된 카페 조회 API 연결되면 cafeName, characterType 쿼리에 저장해두고 쓰기
-export default function OwnerStampBook() {
-  const { selectedCafe } = useSelectedCafe();
+interface TemplateStampBookProps {
+  frontName: string;
+}
 
+export default function TemplateStampBook({ frontName }: TemplateStampBookProps) {
+  const { selectedCafe } = useSelectedCafe();
   const totalStamps = 10;
   const stampedCount = 3;
   const characterType = selectedCafe?.characterType?.toLowerCase() || "yellow";
@@ -15,7 +17,7 @@ export default function OwnerStampBook() {
     <div className="w-[320px] h-[154px] flex flex-col gap-4 py-5 px-4 bg-yellow-300 rounded-lg shadow-sm">
       <div className="flex gap-[6px] items-center">
         <Image src="/icon/coffee.svg" alt="원두" width={16} height={16} />
-        <p className="text-sm font-bold text-[#254434]">{selectedCafe?.cafeName}</p>
+        <p className="text-sm font-bold text-[#254434]">{frontName}</p>
       </div>
       <div className="grid grid-cols-5 gap-x-[20px] gap-y-3 place-items-center">
         {Array.from({ length: totalStamps }).map((_, index) => (
